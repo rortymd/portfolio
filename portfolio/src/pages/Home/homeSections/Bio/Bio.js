534523e6e3ef76ffe2919512f8b33333fe1dba1e
DmitriyR54 <45ryirtimd@gmail.com>
2023-04-27 15:28:51 +0300

    home page. bio section update

--- portfolio/src/pages/Home/homeSections/Bio/Bio.js
+++ portfolio/src/pages/Home/homeSections/Bio/Bio.js
@@ -4,56 +4,46 @@ import scrollTrigger from 'gsap/ScrollTrigger';
 // styles
 import './Bio.scss';
 
-gsap.registerPlugin(scrollTrigger);
+setTimeout(() => {
+    gsap.registerPlugin(scrollTrigger);
 
-const heyLetters = gsap.utils.toArray('.bio__title-hey span');
-const paragraphs = gsap.utils.toArray('.bio__paragraph span');
+    const heyLetters = gsap.utils.toArray('.bio__title-hey span');
+    const paragraphs = gsap.utils.toArray('.bio__paragraph span');
 
-const screenWidth = document.querySelector('.bio__title').offsetWidth;
-
-heyLetters.forEach((span) => {
-    gsap.set(span, {
-        x: () => screenWidth / 2 - span.offsetLeft,
-        opacity: 0,
-        scale: 3,
+    heyLetters.forEach((span) => {
+        gsap.set(span, {
+            x: 300,
+            opacity: 0,
+        });
     });
-});
 
-paragraphs.forEach((span) => {
-    gsap.set(span, {
-        x: 300,
-        opacity: 0,
+    paragraphs.forEach((span) => {
+        gsap.set(span, {
+            x: 300,
+            opacity: 0,
+        });
     });
-});
 
-gsap.timeline({
-    scrollTrigger: {
-        trigger: '.bio__title',
-        start: '50px 70%',
-        end: '+=500 100%',
-        scrub: 2,
-    },
-})
-    .to(heyLetters, {
-        scale: 1,
-        opacity: 1,
-        stagger: 0.5,
-        delay: 0.5,
-        ease: 'power4.out',
-    })
-    .to(
-        heyLetters,
-        {
-            x: 0,
-            stagger: 0.5,
-            delay: 0.5,
+    gsap.timeline({
+        scrollTrigger: {
+            trigger: '.bio__title',
+            start: '50px 70%',
+            end: '+=500 100%',
+            scrub: 2,
         },
-        '<'
-    )
-    .to(paragraphs, {
-        x: 0,
-        opacity: 1,
-        stagger: 1,
-        delay: 0.5,
-        ease: 'power4.out',
-    });
+    })
+        .to(heyLetters, {
+            x: 0,
+            opacity: 1,
+            stagger: 1,
+            delay: 0.5,
+            ease: 'power4.out',
+        })
+        .to(paragraphs, {
+            x: 0,
+            opacity: 1,
+            stagger: 1,
+            delay: 0.5,
+            ease: 'power4.out',
+        });
+}, 50);
