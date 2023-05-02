da1889edd70f61250b2a20436e5da25a76acbbac
DmitriyR54 <45ryirtimd@gmail.com>
2023-05-02 21:12:12 +0300

    home page. bio section. scroll animation update

--- portfolio/src/pages/Home/homeSections/Bio/Bio.js
+++ portfolio/src/pages/Home/homeSections/Bio/Bio.js
@@ -4,21 +4,42 @@ import ScrollTrigger from 'gsap/ScrollTrigger';
 // styles
 import './Bio.scss';
 
-// gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger);
 
-// /* DOM nodes */
-// const title = document.querySelector('.bio .title');
-// const subtitle = document.querySelectorAll('.bio .subtitle');
+/* DOM nodes */
+const section = document.querySelector('.bio');
+const title = document.querySelector('.bio .title');
+const titleLetters = gsap.utils.toArray('.bio .title span');
 
-// gsap.from(title, {
-//     duration: 5,
-//     xPercent: 50,
-//     ease: 'linear',
-//     scrollTrigger: {
-//         trigger: title,
-//         markers: true,
-//         start: 'top 50%',
-//         end: 'bottom 75%',
-//         scrub: 3,
-//     },
-// });
+const subtitle = gsap.utils.toArray('.bio .subtitle span');
+const span = gsap.utils.toArray('.bio span');
+
+const tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: section,
+        markers: true,
+        start: 'top 100',
+        end: '+=6000',
+        scrub: 1,
+        pin: true,
+    },
+    defaults: {
+        ease: 'linear',
+    },
+});
+
+tl.from(titleLetters, {
+    duration: 8,
+    stagger: 4,
+    x: 400,
+    opacity: 0,
+});
+
+tl.from(subtitle, {
+    duration: 8,
+    stagger: 10,
+    x: 400,
+    opacity: 0,
+})
+
+console.log([title, subtitle]);
